bb40ff0ff88166d1220c2a88641277ea7bff5495
eastflag <eastflag@gmail.com>
2014-11-04 20:18:17 +0900

    사고력게임(야구게임) 사운드빼고 로직 완성

--- DaeguProject/src/com/eastflag/silver/fragment/_1_1_Fragment.java
+++ DaeguProject/src/com/eastflag/silver/fragment/_1_1_Fragment.java
@@ -1,99 +1,163 @@
 package com.eastflag.silver.fragment;
 
+import java.util.ArrayList;
+
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.NumberPicker;
-import android.widget.Spinner;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
-import com.androidquery.AQuery;
 import com.eastflag.silver.R;
 
 public class _1_1_Fragment extends Fragment {
+	private final static int MSG_RESET_ANSWER = 1;
+	private final static int MSG_TIMER = 2;
 	
 	private View mView;
-	private NumberPicker picker1, picker2, picker3;
-	private Button btnSubmit;
+	private TextView tvTimer; //타이머
+	private ImageView ivRecord; //기록
+	private ImageView ivHelp;   //도움말
+	private TextView answerOne, answerTwo, answerThree;
+	private TextView answerResult;
+	private TextView number0, number1, number2, number3, number4;
+	private TextView number5, number6, number7, number8, number9;
+	private Button btnDelete, btnSubmit;
+	private Button btnRestart;
+	private LinearLayout rootHistory, rootVictory;
+	
+	private ListView mListView;
+	private ArrayList<History> mHistoryList = new ArrayList<History>(); 
+	private MyAdapter mAdapter;
 	
 	private String mAnswer; 
 	private int mStrike, mBall;
+	private int mHit;
+	private int mTime;
 	
 	public _1_1_Fragment () {
 		
 	}
 	
+	private Handler mHandler = new Handler() {
+
+		@Override
+		public void handleMessage(Message msg) {
+			switch(msg.what) {
+			case MSG_RESET_ANSWER:
+				answerResult.setText("");
+				answerOne.setText("?");
+				answerTwo.setText("?");
+				answerThree.setText("?");
+				break;
+				
+			case MSG_TIMER:
+				++mTime;
+				tvTimer.setText(String.format("%02d:%02d", mTime/60, mTime%60));
+				mHandler.sendEmptyMessageDelayed(MSG_TIMER, 1000);
+				break;
+			}
+		}
+		
+	};
+	
 	View.OnClickListener mClick = new View.OnClickListener() {
 		@Override
 		public void onClick(View v) {
 			switch(v.getId()) {
+			case R.id.numberZero:
+			case R.id.numberOne:
+			case R.id.numberTwo:
+			case R.id.numberThree:
+			case R.id.numberFour:
+			case R.id.numberFive:
+			case R.id.numberSix:
+			case R.id.numberSeven:
+			case R.id.numberEight:
+			case R.id.numberNine:
+				inputNumber(((TextView)v).getText().toString());
+				break;
+				
+			case R.id.btnDelete:
+				deleteNumber();
+				break;
+				
 			case R.id.btnSubmit:
-				mStrike = 0;
-				mBall = 0;
-				String a = String.valueOf(picker1.getValue());
-				String b = String.valueOf(picker2.getValue());
-				String c = String.valueOf(picker3.getValue());
-				if(mAnswer.substring(0, 1).equals(a)) {
-					//strike
-					++mStrike;
-				} else if (mAnswer.contains(a)) {
-					//ball
-					++mBall;
-				}
-				if(mAnswer.substring(1, 2).equals(b)) {
-					//strike
-					++mStrike;
-				} else if (mAnswer.contains(b)) {
-					//ball
-					++mBall;
-				}
-				if(mAnswer.substring(2, 3).equals(c)) {
-					//strike
-					++mStrike;
-				} else if (mAnswer.contains(c)) {
-					//ball
-					++mBall;
-				}
-				Log.d("LDK", "a:" + a + ", b:" + b + ", c:" + c);
-				Log.d("LDK", "" + mStrike + " strike, " + mBall + " ball");
+				checkSubmit();
+				break;
 				
-				checkEnd();
+			case R.id.btnRestart:
+				restartGame();
+				break;
 				
+			case R.id.ivRecord:
+				showRecord();
 				break;
 			}
 		}
 	};
-	
-	private void checkEnd() {
-		if(mStrike == 3) {
-			
-		}
-	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		mView = inflater.inflate(R.layout.fragment_11, null);
 		
-		picker1 = (NumberPicker) mView.findViewById(R.id.numberPicker1);
-		picker2 = (NumberPicker) mView.findViewById(R.id.numberPicker2);
-		picker3 = (NumberPicker) mView.findViewById(R.id.numberPicker3);
+		tvTimer = (TextView) mView.findViewById(R.id.tvTimer);
+		ivRecord = (ImageView) mView.findViewById(R.id.ivRecord);
+		ivHelp = (ImageView) mView.findViewById(R.id.ivHelp);
+		answerOne = (TextView) mView.findViewById(R.id.answerOne);
+		answerTwo = (TextView) mView.findViewById(R.id.answerTwo);
+		answerThree = (TextView) mView.findViewById(R.id.answerThree);
+		answerResult = (TextView) mView.findViewById(R.id.tvResult);
+		number0 = (TextView) mView.findViewById(R.id.numberZero);
+		number1 = (TextView) mView.findViewById(R.id.numberOne);
+		number2 = (TextView) mView.findViewById(R.id.numberTwo);
+		number3 = (TextView) mView.findViewById(R.id.numberThree);
+		number4 = (TextView) mView.findViewById(R.id.numberFour);
+		number5 = (TextView) mView.findViewById(R.id.numberFive);
+		number6 = (TextView) mView.findViewById(R.id.numberSix);
+		number7 = (TextView) mView.findViewById(R.id.numberSeven);
+		number8 = (TextView) mView.findViewById(R.id.numberEight);
+		number9 = (TextView) mView.findViewById(R.id.numberNine);
+		btnDelete = (Button) mView.findViewById(R.id.btnDelete);
 		btnSubmit = (Button) mView.findViewById(R.id.btnSubmit);
+		btnRestart = (Button) mView.findViewById(R.id.btnRestart);
+		rootHistory = (LinearLayout) mView.findViewById(R.id.rootHistory);
+		rootVictory = (LinearLayout) mView.findViewById(R.id.rootVictory);
+		mListView = (ListView) mView.findViewById(R.id.listHistory);
 		
-		picker1.setMaxValue(9);
-		picker1.setMinValue(1);
-		picker2.setMaxValue(9);
-		picker2.setMinValue(1);
-		picker3.setMaxValue(9);
-		picker3.setMinValue(1);
+		ivRecord.setOnClickListener(mClick);
+		ivHelp.setOnClickListener(mClick);
+		number0.setOnClickListener(mClick);
+		number1.setOnClickListener(mClick);
+		number2.setOnClickListener(mClick);
+		number3.setOnClickListener(mClick);
+		number4.setOnClickListener(mClick);
+		number5.setOnClickListener(mClick);
+		number6.setOnClickListener(mClick);
+		number7.setOnClickListener(mClick);
+		number8.setOnClickListener(mClick);
+		number9.setOnClickListener(mClick);
+		btnDelete.setOnClickListener(mClick);
 		btnSubmit.setOnClickListener(mClick);
+		btnRestart.setOnClickListener(mClick);
+		
+		mAdapter = new MyAdapter();
+		mListView.setAdapter(mAdapter);
+		
+		//Timer start
+		mHandler.sendEmptyMessageDelayed(MSG_TIMER, 1000);
 		
 		//Random 3자리 생성
 		int a = getRandomOne();
@@ -114,10 +178,175 @@ public class _1_1_Fragment extends Fragment {
 
 	@Override
 	public void onDestroyView() {
+		mHandler.removeMessages(MSG_TIMER);
 		super.onDestroyView();
 	}
 	
 	private int getRandomOne() {
-		return (int) (Math.random() * 9 + 1);
+		return (int) (Math.random() * 10);
+	}
+	
+	private void inputNumber(String number) {
+		if(answerOne.getText().toString().equals("?")) {
+			answerOne.setText(number);
+		} else if(answerTwo.getText().toString().equals("?")) {
+			answerTwo.setText(number);
+		} else {
+			answerThree.setText(number);
+		}
+	}
+	
+	private void deleteNumber() {
+		if (!answerThree.getText().toString().equals("?")) {
+			answerThree.setText("?");
+		} else if (!answerTwo.getText().toString().equals("?")) {
+			answerTwo.setText("?");
+		} else if (!answerOne.getText().toString().equals("?")) {
+			answerOne.setText("?");
+		}
+	}
+	
+	private void checkSubmit() {
+		String a = String.valueOf(answerOne.getText().toString());
+		String b = String.valueOf(answerTwo.getText().toString());
+		String c = String.valueOf(answerThree.getText().toString());
+		
+		if(a.equals("?") || b.equals("?") || c.equals("?")) {
+			answerResult.setText("3자리를 모두 입력하세요");
+			return;
+		}
+		
+		if(a.equals(b) || a.equals(c) || b.equals(c)) {
+			answerResult.setText("각각 다른 3자리를 입력하세요.");
+			return;
+		}
+
+		mStrike = 0;
+		mBall = 0;
+		
+		if(mAnswer.substring(0, 1).equals(a)) {
+			//strike
+			++mStrike;
+		} else if (mAnswer.contains(a)) {
+			//ball
+			++mBall;
+		}
+		if(mAnswer.substring(1, 2).equals(b)) {
+			//strike
+			++mStrike;
+		} else if (mAnswer.contains(b)) {
+			//ball
+			++mBall;
+		}
+		if(mAnswer.substring(2, 3).equals(c)) {
+			//strike
+			++mStrike;
+		} else if (mAnswer.contains(c)) {
+			//ball
+			++mBall;
+		}
+		Log.d("LDK", "a:" + a + ", b:" + b + ", c:" + c);
+		Log.d("LDK", "" + mStrike + " strike, " + mBall + " ball");
+		
+		checkEnd();
+	}
+	
+	private void checkEnd() {
+		String input = answerOne.getText().toString() + answerTwo.getText().toString() + answerThree.getText().toString();
+		String result = String.format("%dS %dB", mStrike, mBall);
+		answerResult.setText(result);
+		mHistoryList.add(new History(++mHit, input, result));
+		mAdapter.notifyDataSetChanged();
+		//리스트뷰의 맨 마지막으로 스크롤
+		mListView.post(new Runnable() {
+	        @Override
+	        public void run() {
+	            mListView.setSelection(mAdapter.getCount() - 1);
+	        }
+	    });
+		
+		if(mStrike != 3) {
+			//answerResult 삭제 및 초기화
+			mHandler.sendEmptyMessageDelayed(MSG_RESET_ANSWER, 1000);
+		} else {
+			//게임 오버 처리
+			mHandler.removeMessages(MSG_TIMER);
+			rootHistory.setVisibility(View.INVISIBLE);
+			rootVictory.setVisibility(View.VISIBLE);
+			//랭킹스코어 기록
+		}
+	}
+	
+	private void restartGame() {
+		answerResult.setText("");
+		answerOne.setText("?");
+		answerTwo.setText("?");
+		answerThree.setText("?");
+		
+		mHit = 0;
+		mTime = 0;
+		mHistoryList.clear();
+		mAdapter.notifyDataSetChanged();
+		rootHistory.setVisibility(View.VISIBLE);
+		rootVictory.setVisibility(View.INVISIBLE);
+	}
+	
+	private void showRecord() {
+		View view = View.inflate(getActivity(), R.layout.fragment_11_record, null);
+		ListView listview = (ListView) view.findViewById(R.id.listRecord);
+		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		builder.setView(view)
+			.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					
+				}
+			})
+			.create().show();
+	}
+	
+	class History {
+		int hit;
+		String input;
+		String result;
+		
+		public History(int hit, String input, String result) {
+			this.hit = hit;
+			this.input = input;
+			this.result = result;
+		}
+	}
+	
+	class MyAdapter extends BaseAdapter {
+		@Override
+		public int getCount() {
+			return mHistoryList.size();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return mHistoryList.get(position);
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			if(convertView == null) {
+				convertView = View.inflate(getActivity(), R.layout.fragment_11_history, null);
+			}
+			TextView tvHit = (TextView) convertView.findViewById(R.id.tvHit);
+			TextView tvInput = (TextView) convertView.findViewById(R.id.tvInput);
+			TextView tvResult = (TextView) convertView.findViewById(R.id.tvResult);
+			
+			tvHit.setText(String.valueOf(mHistoryList.get(position).hit));
+			tvInput.setText(mHistoryList.get(position).input);
+			tvResult.setText(mHistoryList.get(position).result);
+			
+			return convertView;
+		}
 	}
 }
