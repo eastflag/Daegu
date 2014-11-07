ee19d18f12277148c90d9668572786adb7634ebc
eastflag <eastflag@gmail.com>
2014-11-07 21:15:38 +0900

    스마트폰 사용설명 메뉴 추가 메인 메뉴 구조 변경

--- DaeguProject/src/com/eastflag/silver/MainActivity.java
+++ DaeguProject/src/com/eastflag/silver/MainActivity.java
@@ -1,5 +1,7 @@
 package com.eastflag.silver;
 
+import java.util.HashMap;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Fragment;
@@ -21,14 +23,14 @@ import com.eastflag.silver.fragment._4_2_Fragment;
 import com.eastflag.silver.fragment._4_3_Fragment;
 
 public class MainActivity extends Activity {
-
-	private static final int FRAGMENT_11 = 11;
-	private static final int FRAGMENT_12 = 12;
-	private static final int FRAGMENT_21 = 21;
-	private static final int FRAGMENT_31 = 31;
-	private static final int FRAGMENT_41 = 41;
-	private static final int FRAGMENT_42 = 42;
-	private static final int FRAGMENT_43 = 43;
+	
+	private final static int FRAGMENT_11 = 11;
+	private final static int FRAGMENT_12 = 12;
+	private final static int FRAGMENT_21 = 21;
+	private final static int FRAGMENT_31 = 31;
+	private final static int FRAGMENT_41 = 41;
+	private final static int FRAGMENT_42 = 42;
+	private final static int FRAGMENT_43 = 43;
 
 	private FragmentManager mFm;
 	private Fragment mFragment;
@@ -37,30 +39,26 @@ public class MainActivity extends Activity {
 	private LinearLayout mTabGroup;
 	private TextView tab_1, tab_2;
 	
+	private HashMap<Integer, String> mInfo = new HashMap<Integer, String>();
+	
 	View.OnClickListener mClick = new View.OnClickListener() {
 		@Override
 		public void onClick(View v) {
 			switch(v.getId()) {
 			case R.id.ll_1:
 				showSubmenu(FRAGMENT_11);
-				tab_1.setTag(FRAGMENT_11);
-				tab_2.setTag(FRAGMENT_12);
 				break;
 				
 			case R.id.ll_2:
 				showSubmenu(FRAGMENT_21);
-				tab_1.setTag(FRAGMENT_21);
 				break;
 				
 			case R.id.ll_3:
 				showSubmenu(FRAGMENT_31);
-				tab_1.setTag(FRAGMENT_31);
 				break;
 				
 			case R.id.ll_4:
 				showSubmenu(FRAGMENT_41);
-				tab_1.setTag(FRAGMENT_41);
-				tab_2.setTag(FRAGMENT_42);
 				break;
 			}
 		}
@@ -72,15 +70,19 @@ public class MainActivity extends Activity {
 			int type = (Integer) v.getTag();
 			switch(type) {
 			case FRAGMENT_11:
+				if(mFragment.getTag().equals(String.valueOf(FRAGMENT_11))) return;
 				showSubmenu(FRAGMENT_11);
 				break;
 			case FRAGMENT_12:
+				if(mFragment.getTag().equals(String.valueOf(FRAGMENT_12))) return;
 				showSubmenu(FRAGMENT_12);
 				break;
 			case FRAGMENT_21:
+				if(mFragment.getTag().equals(String.valueOf(FRAGMENT_21))) return;
 				showSubmenu(FRAGMENT_21);
 				break;
 			case FRAGMENT_31:
+				if(mFragment.getTag().equals(String.valueOf(FRAGMENT_31))) return;
 				showSubmenu(FRAGMENT_31);
 				break;
 			case FRAGMENT_41:
@@ -100,6 +102,14 @@ public class MainActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mInfo.put(FRAGMENT_11, "사고력 게임");
+		mInfo.put(FRAGMENT_12, "기억력 게임");
+		mInfo.put(FRAGMENT_21, "유용한 앱정보");
+		mInfo.put(FRAGMENT_31, "스마트폰 사용설명");
+		mInfo.put(FRAGMENT_41, "방사능 정보");
+		mInfo.put(FRAGMENT_42, "미세먼지 정보");
+		mInfo.put(FRAGMENT_43, "자외선 지수");
 
 		mMainMenu = (LinearLayout) findViewById(R.id.main_menu);
 		mTabGroup = (LinearLayout) findViewById(R.id.tab_group);
@@ -179,43 +189,41 @@ public class MainActivity extends Activity {
 		case FRAGMENT_11:
 			mFragment = new _1_1_Fragment();
 			mFm.beginTransaction().replace(R.id.container, mFragment, String.valueOf(FRAGMENT_11)).commit();
-			refreshTab(0, "사고력 게임", "기억력 게임");
+			refreshTab(0, FRAGMENT_11, FRAGMENT_12);
 			break;
 			
 		case FRAGMENT_12:
 			mFragment = new _1_2_Fragment();
 			mFm.beginTransaction().replace(R.id.container, mFragment, String.valueOf(FRAGMENT_12)).commit();
-			refreshTab(1, "사고력 게임", "기억력 게임");
+			refreshTab(1, FRAGMENT_11, FRAGMENT_12);
 			break;
 
 		case FRAGMENT_21:
 			mFragment = new _2_1_Fragment();
 			mFm.beginTransaction().replace(R.id.container, mFragment, String.valueOf(FRAGMENT_21)).commit();
-			refreshTab(0, "유용한 앱정보");
+			refreshTab(0, FRAGMENT_21);
 			break;
 			
 		case FRAGMENT_31:
 			mFragment = new _3_1_Fragment();
 			mFm.beginTransaction().replace(R.id.container, mFragment, String.valueOf(FRAGMENT_31)).commit();
-			refreshTab(0, "스마트폰 사용설명");
+			refreshTab(0, FRAGMENT_31);
 			break;
 			
 		case FRAGMENT_41:
 			mFragment = new _4_1_Fragment();
 			mFm.beginTransaction().replace(R.id.container, mFragment, String.valueOf(FRAGMENT_41)).commit();
-			tab_1.setText("");
+			refreshTab(0, FRAGMENT_41);
 			break;
 			
 		case FRAGMENT_42:
 			mFragment = new _4_2_Fragment();
 			mFm.beginTransaction().replace(R.id.container, mFragment, String.valueOf(FRAGMENT_42)).commit();
-			tab_1.setText("");
 			break;
 			
 		case FRAGMENT_43:
 			mFragment = new _4_3_Fragment();
 			mFm.beginTransaction().replace(R.id.container, mFragment, String.valueOf(FRAGMENT_43)).commit();
-			tab_1.setText("");
 			break;
 		}
 	}
@@ -226,19 +234,24 @@ public class MainActivity extends Activity {
 	}
 	
 	//선택된 탭의 위치(인덱스는 0부터), 모든 탭의 스트링
-	private void refreshTab(int positionOfOn, String ... str) {
-		if(str.length == 2) {
-			tab_1.setText(str[0]);
-			tab_2.setText(str[1]);
+	private void refreshTab(int positionOfOn, int ... position) {
+		if(position.length == 2) {
+			tab_1.setText(mInfo.get(position[0]));
+			tab_2.setText(mInfo.get(position[1]));
+			tab_1.setTag(position[0]);
+			tab_2.setTag(position[1]);
 			if(positionOfOn == 0) {
 				tab_1.setBackgroundResource(R.drawable.tab_s_on);
-				tab_1.setVisibility(View.VISIBLE);
+				tab_2.setBackgroundResource(R.drawable.tab_s_off);
 			} else {
 				tab_1.setBackgroundResource(R.drawable.tab_s_off);
-				tab_2.setVisibility(View.VISIBLE);
+				tab_2.setBackgroundResource(R.drawable.tab_s_on);
 			}
+			tab_1.setVisibility(View.VISIBLE);
+			tab_2.setVisibility(View.VISIBLE);
 		} else {
-			tab_1.setText(str[0]);
+			tab_1.setText(mInfo.get(position[0]));
+			tab_1.setTag(position[0]);
 			tab_1.setBackgroundResource(R.drawable.tab_s_on);
 			tab_1.setVisibility(View.VISIBLE);
 			tab_2.setBackgroundResource(R.drawable.tab_s_off);
